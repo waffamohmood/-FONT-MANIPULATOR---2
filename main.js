82b60e17202bcc0612649a9cac00a09aa6470877
Hassah Menar <86717596+waffamohmood@users.noreply.github.com>
2023-05-05 10:57:14 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX=0;
+noseY=0;
+
+
+
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+  video.hide();
+  
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+  
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+  }
+  
+  function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+  }
+  
+  
+  function gotPoses(results)
+  {
+    if(results.length > 0)
+    {
+      console.log(results);
+      noseX = results[0].pose.nose.x;
+      noseY = results[0].pose.nose.y;
+      console.log("noseX = " + noseX +" noseY = " + noseY);
+    }
+}
+
+function draw() {
+    background('#969A97');
+    
+      
+      
+    }
